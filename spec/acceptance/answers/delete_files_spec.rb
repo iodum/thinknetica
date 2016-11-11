88acef4759cd0aa9049ffbe4490a3926919f985c
Olya <violetiodum@gmail.com>
2016-11-11 21:55:25 +0300

    delete files from answers list

--- spec/acceptance/answers/delete_files_spec.rb
+++ spec/acceptance/answers/delete_files_spec.rb
@@ -14,6 +14,21 @@ feature 'Delete files', %q{
       sign_in(user)
       visit question_path answer.question
     end
+
+    scenario 'Authenticated user delete own file', js: true  do
+      within '.answer-wrapper' do
+        expect(page).to have_selector '.glyphicon-remove'
+      end
+    end
+
+    scenario 'Authenticated user delete own file', js: true  do
+      within '.answer-wrapper .attachments' do
+        click_on 'delete'
+      end
+
+      expect(page).to_not have_select '.answer-wrapper .attachments'
+    end
+
     scenario 'Authenticated user delete own file', js: true  do
       within '.answer-wrapper' do
         click_on 'Edit'
@@ -35,21 +50,35 @@ feature 'Delete files', %q{
     end
   end
 
-  scenario 'Authenticated user can\'t delete other user\'s files', js: true  do
-    new_user = create(:user)
-    sign_in(new_user)
-    visit question_path answer.question
-    within '.answer-wrapper' do
-      expect(page).to_not have_link 'Edit'
-      expect(page).to_not have_link 'Delete'
+  describe 'Authenticated user' do
+    before do
+      new_user = create(:user)
+      sign_in(new_user)
+      visit question_path answer.question
+    end
+
+    scenario 'can\'t delete other user\'s files', js: true  do
+      within '.answer-wrapper' do
+        expect(page).to_not have_selector '.glyphicon-remove'
+      end
+    end
+
+    scenario 'can\'t delete other user\'s files in edit form', js: true  do
+      within '.answer-wrapper' do
+        expect(page).to_not have_link 'Edit'
+        expect(page).to_not have_link 'Delete'
+      end
     end
   end
 
   scenario 'Non-authenticated user can\'t delete any file', js: true  do
     visit question_path answer.question
-    within '.question-wrapper' do
+    within '.answer-wrapper' do
       expect(page).to_not have_link 'Edit'
-      expect(page).to_not have_link 'Delete'
+      within '.attachments' do
+        expect(page).to_not have_link 'Delete'
+        expect(page).to_not have_selector '.glyphicon-remove'
+      end
     end
   end
 
