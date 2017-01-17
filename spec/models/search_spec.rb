0dacdf8fc9bf4c24603d5321052741918dcbfaa9
Olya <violetiodum@gmail.com>
2017-01-17 17:45:48 +0300

    fix

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe Search, type: :model do
   describe '.search' do
     %w(question answer comment user).each do |resource|
       it "call search method for #{resource}s" do
-        expect(resource.classify.constantize).to receive(:search).with('')
-        Search.search('', resource)
+        expect(resource.classify.constantize).to receive(:search).with('123')
+        Search.search('123', resource)
       end
     end
 
     it 'should call global search' do
-      expect(ThinkingSphinx).to receive(:search).with('')
-      Search.search('', 'all')
+      expect(ThinkingSphinx).to receive(:search).with('123')
+      Search.search('123', 'all')
     end
 
     it 'not valid value' do
-      expect(ThinkingSphinx).to receive(:search).with('')
-      Search.search('', 'not_valid_value')
+      expect(ThinkingSphinx).to receive(:search).with('123')
+      Search.search('123', 'not_valid_value')
     end
   end
 
